41dbe2ce07eb6c28b173e90ad189f043579c2864
ChrysaLynn <chrysalis.parker@gmail.com>
2021-12-28 19:17:04 -0800

    Added first function

--- js/script.js
+++ js/script.js
@@ -18,6 +18,27 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage(list, page) {
+   let startIndex = (page * 9) - 9;
+   let endIndex = page * 9;
+   let studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+   for (let i =0; i < list.length; i++) {
+      if (i >= startIndex, i < endIndex) {
+         let studentItem = `<li class="student-item cf">
+         <div class="student-details">
+         <img class="avatar" src="${data[i].picture.large}">
+         <h3>${data[i].name.first} ${data[i].name.last}</h3>
+         <span class="email">${data[i].email}</span>
+         </div>
+         <div class="joined-details">
+         <span class="date">${data[i].registered.date}</span>
+         </div>`
+         studentList.insertAdjacentHTML('beforeend', studentItem);
+      }
+   }
+} 
+showPage(data, 1);
 
 
 /*
